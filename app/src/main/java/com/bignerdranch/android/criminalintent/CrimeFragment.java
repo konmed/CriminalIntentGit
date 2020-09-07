65e31eacdcea76fd273b8edca6297d0209804d7e
Konstatntin <Konstantinmedvedev0@gmail.com>
2020-09-07 21:45:07 +0400

    all

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,26 +1,31 @@
 package com.bignerdranch.android.criminalintent;
-
-
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import static android.widget.CompoundButton.*;
+
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
-
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+
     }
 
     @Nullable
@@ -42,6 +47,17 @@ public class CrimeFragment extends Fragment {
             public void afterTextChanged(Editable s) {
                 // И здесь тоже
                 }
+        });
+        mDateButton = v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean ischeked) {
+                mCrime.setSolved(ischeked);
+            }
         });
 
         return v;
